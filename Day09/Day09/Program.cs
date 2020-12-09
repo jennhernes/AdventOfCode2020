9cc58269b15e610e862984af9158de14040e50a7
Jenn Hernes <jenn_hernes@hotmail.com>
2020-12-09 08:54:15 +0200

    add complete solution for Day 9

--- Day09/Day09/Program.cs
+++ Day09/Day09/Program.cs
@@ -9,16 +9,16 @@ namespace Day09
     {
         static void Main(string[] args)
         {
-            Part1();
-            // Part2();
+            // Part1();
+            Part2();
         }
 
         static void Part1()
         {
-            string filename = "../../../../test.txt";
+            string filename = "../../../../input.txt";
             StreamReader sr = new StreamReader(filename);
             List<A> prevNum = new List<A>();
-            int len = 5;
+            int len = 25;
 
             int newNum = 0;
             bool valid;
@@ -30,12 +30,12 @@ namespace Day09
                 if (numInstr > len)
                 {
                     valid = false;
-                    if (newNum > prevNum[^1].num)
+                    if (newNum > prevNum[^1].num * 2)
                         break;
 
-                    int low = prevNum.Count / 2;
-                    int high = low + 1;
-                    while (low >= 0 && high < prevNum.Count)
+                    int low = 0;
+                    int high = prevNum.Count - 1;
+                    while (low < high)
                     {
                         int sum = prevNum[low].num + prevNum[high].num;
                         if (sum == newNum)
@@ -45,11 +45,11 @@ namespace Day09
                         }
                         else if (sum > newNum)
                         {
-                            low--;
+                            high--;
                         }
                         else
                         {
-                            high++;
+                            low++;
                         }
                     }
                     if (valid)
@@ -57,21 +57,61 @@ namespace Day09
                         int lastLine = prevNum.Min(x => x.line);
                         prevNum.RemoveAll(x => x.line == lastLine);
                         prevNum.Add(new A(newNum, numInstr));
-                        prevNum.Sort()
+                        prevNum.Sort();
+                    }
+                    else
+                    {
+                        break;
                     }
                 }
                 else
                 {
                     prevNum.Add(new A(newNum, numInstr));
-                    prevNum = (List<A>)prevNum.OrderBy(x => x.num);
+                    prevNum.Sort();
                 }
                 numInstr++;
             }
             Console.WriteLine(newNum);
         }
+
+        static void Part2()
+        {
+            string filename = "../../../../input.txt";
+            StreamReader sr = new StreamReader(filename);
+            List<long> allInstr = new List<long>();
+            long problem = 88311122;
+
+            long answer = 0;
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                allInstr.Add(Convert.ToInt64(line));
+                for (int i = 0; i < allInstr.Count; i++)
+                {
+                    long contSum = 0;
+                    long min = long.MaxValue;
+                    long max = long.MinValue;
+                    for (int j = i; j < allInstr.Count; j++)
+                    {
+                        contSum += allInstr[j];
+                        min = Math.Min(min, allInstr[j]);
+                        max = Math.Max(max, allInstr[j]);
+                        if (contSum == problem)
+                        {
+                            answer = min + max;
+                            goto end;
+                        }
+                        if (contSum > problem)
+                            break;
+                    }
+                }
+            }
+        end:
+            Console.WriteLine(answer);
+        }
     }
 
-    class A
+    class A : IComparable
     {
         public int num;
         public int line;
@@ -80,6 +120,12 @@ namespace Day09
         {
             this.num = num;
             this.line = line;
+        }
+
+        public int CompareTo(object obj)
+        {
+            A a = obj as A;
+            return this.num.CompareTo(a.num);
         }
     }
 }
