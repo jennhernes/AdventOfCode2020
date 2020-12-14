b02ad6b523f1f67b9ed1f79b3af86d103437a3db
Jenn Hernes <jenn_hernes@hotmail.com>
2020-12-14 10:33:00 +0200

    start Day 13

---
+++ Day13/Day13/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            string filename = "../../../../input.txt";
+            StreamReader sr = new StreamReader(filename);
+
+            string line;
+            int earliest;
+            line = sr.ReadLine();
+            earliest = int.Parse(line);
+            line = sr.ReadLine();
+            List<int> busses = new List<int>();
+            var tokens = line.Split(',');
+            foreach(string s in tokens)
+            {
+                if (s != "x")
+                    busses.Add(int.Parse(s));
+            }
+
+            int minTime = int.MaxValue;
+            int busID = 0;
+            foreach(int b in busses)
+            {
+                if (b - (earliest % b) < minTime)
+                {
+                    minTime = b - (earliest % b);
+                    busID = b;
+                }
+            }
+
+            Console.WriteLine(minTime * busID);
+        }
+
+        static void Part2()
+        {
+            string filename = "../../../../input.txt";
+            StreamReader sr = new StreamReader(filename);
+
+            string line;
+            line = sr.ReadLine();
+            line = sr.ReadLine();
+            Dictionary<int, int> busses = new Dictionary<int, int>();
+            var tokens = line.Split(',');
+            int offset = 0;
+            foreach (string s in tokens)
+            {
+                if (s != "x")
+                    busses.Add(offset, int.Parse(s));
+
+                offset++;
+            }
+
+            long multiple = 1;
+            bool found = false;
+            long time = 0;
+            while (!found)
+            {
+                time = busses[0] * multiple;
+                found = true;
+                foreach (KeyValuePair<int, int> kvp in busses)
+                {
+                    if ((kvp.Value - (time % kvp.Value)) % kvp.Value != kvp.Key)
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+                multiple++;
+            }
+
+            Console.WriteLine(time);
+        }
+    }
+}
