95fe15e2b56b8bdb6b84cf0394cf10fbdc771c12
Jenn Hernes <jenn_hernes@hotmail.com>
2020-12-15 23:14:55 +0200

    add complete solution for Day 15
    saw a comment that mentioned using a Dictionary and OBVIOUSLY THAT'S BETTER THAN A GIANT LIST YA DUNCE
    fun to see the power of using hash over a regular list, and storing each number only once with it's last occurence

---
+++ Day15/Day15/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            List<int> nums = new List<int>() { 0, 3, 1, 6, 7, 5 };
+            // List<int> nums = new List<int>() { 0, 3, 6 };
+            int start = nums.Count;
+            for (int i = start; i < 2020; i++)
+            {
+                var index = nums.FindLastIndex(nums.Count - 2, x => x == nums[nums.Count - 1]);
+                if (index > -1)
+                {
+                    nums.Add(nums.Count - index - 1);
+                }
+                else
+                {
+                    nums.Add(0);
+                }
+            }
+
+            Console.WriteLine(nums[2019]);
+        }
+
+        static void Part2()
+        {
+            var final = 30000000;
+            var nums = new Dictionary<long, long>();
+            nums.Add(0, 0);
+            nums.Add(3, 1);
+            nums.Add(1, 2);
+            nums.Add(6, 3);
+            nums.Add(7, 4);
+            var nextNum = 5L;
+            // var nums = new List<long>() { 0, 3, 6 };
+            //nums.Add(0, 0);
+            //nums.Add(3, 1);
+            //var nextNum = 6L;
+            int start = nums.Count;
+            for (long i = start; i < final; i++)
+            {
+                if (nums.ContainsKey(nextNum))
+                {
+                    var temp = i - nums[nextNum];
+                    nums[nextNum] = i;
+                    nextNum = temp;
+                }
+                else
+                {
+                    nums.Add(nextNum, i);
+                    nextNum = 0;
+                }
+            }
+
+            foreach(KeyValuePair<long, long> kvp in nums)
+            {
+                if (kvp.Value == final - 1)
+                    Console.WriteLine(kvp.Key);
+            }
+        }
+    }
+}
